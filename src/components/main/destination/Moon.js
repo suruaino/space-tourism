afa433fef3f77d339207237e648e63da3caff006
SURU EMMANUEL <suruaino@gmail.com>
2024-05-07 23:15:26 +0100

    started moon styling

--- src/components/main/destination/Moon.js
+++ src/components/main/destination/Moon.js
@@ -1,40 +1,39 @@
+import moon from "./destination-assets/image-moon.png";
 const Moon = () => {
   return (
     <div className="moon">
       <h2>01 Pick your destination</h2>
       <div className="content-bx">
         <div className="left">
-          <img src="" />
+          <img src={moon} />
+        </div>
+        <div className="right">
+          <ul className="tab-bx">
+            <li>Moon</li>
+            <li>Mars</li>
+            <li>Europa</li>
+            <li>Titan</li>
+          </ul>
+          <h2>Moon</h2>
+          <p>
+            See our planet as you’ve never seen it before. A perfect relaxing
+            trip away to help regain perspective and come back refreshed. While
+            you’re there, take in some history by visiting the Luna 2 and Apollo
+            11 landing sites.
+          </p>
+          <div className="more-details">
+            <div className="left">
+              <span className="avg-distance">avg. distance</span>
+              <span className="avg-distance__value">384,400km</span>
+            </div>
 
-          <div className="right">
-            <ul className="tab-bx">
-              <li>Moon</li>
-              <li>Mars</li>
-              <li>Europa</li>
-              <li>Titan</li>
-            </ul>
-            <h2>Moon</h2>
-            <p>
-              See our planet as you’ve never seen it before. A perfect relaxing
-              trip away to help regain perspective and come back refreshed.
-              While you’re there, take in some history by visiting the Luna 2
-              and Apollo 11 landing sites.
-            </p>
-            <div className="more-details">
-              <div className="left">
-                <span className="avg-distance">avg. distance</span>
-                <span className="avg-distance__value">384,400km</span>
-              </div>
-
-              <div className="right">
-                <span className="avg-distance">est. travel time</span>
-                <span className="avg-distance__value">3 days</span>
-              </div>
+            <div className="right">
+              <span className="avg-distance">est. travel time</span>
+              <span className="avg-distance__value">3 days</span>
             </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 };
