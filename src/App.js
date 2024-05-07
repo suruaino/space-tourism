866c21dd6f4c76d17cf99e52678bd89ccb06abc7
SURU EMMANUEL <suruaino@gmail.com>
2024-05-07 13:10:54 +0100

    added the Moon component skeleton

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/main/Home";
+import Moon from "./components/main/destination/Moon"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Moon />
     </div>
   );
 }
--- src/components/main/destination/Moon.js
+++ src/components/main/destination/Moon.js
@@ -1,31 +1,42 @@
+const Moon = () => {
+  return (
+    <div className="moon">
+      <h2>01 Pick your destination</h2>
+      <div className="content-bx">
+        <div className="left">
+          <img src="" />
 
-const Moon= () => {
-    return (
-      <div className="moon">
+          <div className="right">
+            <ul className="tab-bx">
+              <li>Moon</li>
+              <li>Mars</li>
+              <li>Europa</li>
+              <li>Titan</li>
+            </ul>
+            <h2>Moon</h2>
+            <p>
+              See our planet as you’ve never seen it before. A perfect relaxing
+              trip away to help regain perspective and come back refreshed.
+              While you’re there, take in some history by visiting the Luna 2
+              and Apollo 11 landing sites.
+            </p>
+            <div className="more-details">
+              <div className="left">
+                <span className="avg-distance">avg. distance</span>
+                <span className="avg-distance__value">384,400km</span>
+              </div>
 
-  00 Home
-  01 Destination
-  02 Crew
-  03 Technology
+              <div className="right">
+                <span className="avg-distance">est. travel time</span>
+                <span className="avg-distance__value">3 days</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-  01 Pick your destination
+    </div>
+  );
+};
 
-  Moon
-  Mars
-  Europa
-  Titan
-
-  Moon
-
-  See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
-  regain perspective and come back refreshed. While you’re there, take in some history 
-  by visiting the Luna 2 and Apollo 11 landing sites.
-
-  Avg. distance
-  384,400 km
-
-  Est. travel time
-  3 days
-  </div>
-)
-}
+export default Moon;
