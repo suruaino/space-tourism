b34d1212d0b31ec5beed69f8033f4b4971ebbc44
SURU EMMANUEL <suruaino@gmail.com>
2024-05-08 18:40:56 +0100

    started crew styling

--- src/components/main/crew/Crew.js
+++ src/components/main/crew/Crew.js
@@ -3,27 +3,30 @@ const Crew = () => {
   return (
     <div className="crew">
       <div className="commander-container">
-        <div className="left">
+        <div className="content-bx">
           <h2>
             <span>02 </span>meet your crew
           </h2>
 
-          <h3>
-            <span>Commander</span>Douglas Hurley
-          </h3>
+          <div className="left">
+            <h3>
+              <span>Commander</span>Douglas Hurley
+            </h3>
 
-          <p>
-            Douglas Gerald Hurley is an American engineer, former Marine Corps
-            pilot and former NASA astronaut. He launched into space for the
-            third time as commander of Crew Dragon Demo-2.
-          </p>
+            <p>
+              Douglas Gerald Hurley is an American engineer, former Marine Corps
+              pilot and former NASA astronaut. He launched into space for the
+              third time as commander of Crew Dragon Demo-2.
+            </p>
 
-          <div className="indicator">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
+            
           </div>
+          <div className="indicator">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
         </div>
         <div className="right">
           <img src={douglas} />
