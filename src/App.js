56d13ce63a4e491e0e297fb070ad8d085a16d001
SURU EMMANUEL <suruaino@gmail.com>
2024-05-09 21:50:25 +0100

    added technology skeleton

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Home from "./components/main/Home";
 import Moon from "./components/main/destination/Moon";
 import Crew from "./components/main/crew/Crew";
+import Technology from './components/main/technology/Technology';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <Moon />
       <Crew />
+      <Technology />
     </div>
   );
 }
---
+++ src/components/main/technology/Technology.js
@@ -0,0 +1,29 @@
+// import imageLaunch from "./technology-assets/image-launch-vehicle-landscape.jpg";
+const Technology = () => {
+  return (
+    <div className="technology">
+      <div className="launch-vehicle-container">
+        <h2>
+          <span>03</span> Space launch 101
+        </h2>
+        <div className="indicator">
+          <span>1</span>
+          <span>2</span>
+          <span>3</span>
+        </div>
+        <h3>
+          <span>The terminology...</span>Spaceport
+        </h3>
+
+        <p>
+          A spaceport or cosmodrome is a site for launching (or receiving)
+          spacecraft, by analogy to the seaport for ships or airport for
+          aircraft. Based in the famous Cape Canaveral, our spaceport is ideally
+          situated to take advantage of the Earth’s rotation for launch.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Technology;
