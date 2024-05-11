2712bd407743ea0a16422a3ceec776f455120606
SURU EMMANUEL <suruaino@gmail.com>
2024-05-11 21:39:08 +0100

    worked on the crew component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from "./components/header/Header";
 import Home from "./components/main/home/Home";
-// import Destination from "./components/main/destination/Destination";
 import Crew from "./components/main/crew/Crew";
 import Technology from './components/main/technology/Technology';
 import Destination from './components/main/destination/Destination';
--- src/components/main/crew/Crew.js
+++ src/components/main/crew/Crew.js
@@ -1,4 +1,7 @@
 import douglas from "./crew-assets/image-douglas-hurley.png";
+import Mark  from "./crew-assets/image-mark-shuttleworth.png";
+import Victor  from "./crew-assets/image-victor-glover.png";
+import Anousheh  from "./crew-assets/image-anousheh-ansari.png";
 import "./crew.css";
 const Crew = () => {
   return (
@@ -33,6 +36,97 @@ const Crew = () => {
           <img src={douglas} />
         </div>
       </div>
+
+      <div className="mission-specialist-container">
+        <div className="content-bx">
+          <h2>
+            <span>02 </span>meet your crew
+          </h2>
+
+          <div className="left">
+            <h3>
+              <span>Mission Specialist</span>MARK SHUTTLEWORTH
+            </h3>
+
+            <p>
+            Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.
+            </p>
+
+            
+          </div>
+          <div className="indicator">
+              <button></button>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+        </div>
+        <div className="right">
+          <img src={Mark} />
+        </div>
+      </div>
+
+
+      <div className="pilot-container">
+        <div className="content-bx">
+          <h2>
+            <span>02 </span>meet your crew
+          </h2>
+
+          <div className="left">
+            <h3>
+              <span>Pilot</span>Victor Glover
+            </h3>
+
+            <p>
+            Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. 
+            </p>
+
+            
+          </div>
+          <div className="indicator">
+              <button></button>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+        </div>
+        <div className="right">
+          <img src={Victor} />
+        </div>
+      </div>
+
+
+      <div className="flight-engineer-container">
+        <div className="content-bx">
+          <h2>
+            <span>02 </span>meet your crew
+          </h2>
+
+          <div className="left">
+            <h3>
+              <span>Flight Engineer</span>Anousheh Ansari
+            </h3>
+
+            <p>
+            Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.  
+            </p>
+
+            
+          </div>
+          <div className="indicator">
+              <button></button>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+        </div>
+        <div className="right">
+          <img src={Anousheh} />
+        </div>
+      </div>
+
+
     </div>
   );
 };
