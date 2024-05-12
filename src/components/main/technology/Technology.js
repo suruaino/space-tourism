9bac4d087a2702c0538c219d53eb385e9a69aa1d
SURU EMMANUEL <suruaino@gmail.com>
2024-05-12 15:18:41 +0100

    worked on technology component

--- src/components/main/technology/Technology.js
+++ src/components/main/technology/Technology.js
@@ -1,19 +1,21 @@
-import imageLaunch from "./technology-assets/image-launch-vehicle-landscape.jpg";
+import imageLaunch from "./technology-assets/image-launch-vehicle-portrait.jpg";
+import spaceport from "./technology-assets/image-spaceport-portrait.jpg";
+import capsule from "./technology-assets/image-space-capsule-portrait.jpg";
 import "./technology.css";
 
 const Technology = () => {
   return (
     <div className="technology">
-      <div className="launch-vehicle-container">
+      <div className="launch-container">
         <h2>
           <span>03</span> Space launch 101
         </h2>
-        <div className="content-bx">
+        <div className="launch__content-bx">
           <div className="content">
             <div className="content__indicator">
-              <span>1</span>
-              <span>2</span>
-              <span>3</span>
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
             </div>
             <div className="content__details">
               <h3>
@@ -30,6 +32,64 @@ const Technology = () => {
           </div>
         </div>
       </div>
+
+
+      <div className="spaceport-container">
+        <h2>
+          <span>03</span> Space launch 101
+        </h2>
+        <div className="spaceport__content-bx">
+          <div className="content">
+            <div className="content__indicator">
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+            </div>
+            <div className="content__details">
+              <h3>
+                <span>The terminology...</span>spaceport
+              </h3>
+
+              <p>
+              A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.
+              </p>
+            </div>
+          </div>
+          <div className="img-bx">
+            <img src={spaceport} />
+          </div>
+        </div>
+      </div>
+
+
+      <div className="capsule-container">
+        <h2>
+          <span>03</span> Space launch 101
+        </h2>
+        <div className="capsule__content-bx">
+          <div className="content">
+            <div className="content__indicator">
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+            </div>
+            <div className="content__details">
+              <h3>
+                <span>The terminology...</span>space capsule
+              </h3>
+
+              <p>
+              A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.
+              </p>
+            </div>
+          </div>
+          <div className="img-bx">
+            <img src={capsule} />
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 };
