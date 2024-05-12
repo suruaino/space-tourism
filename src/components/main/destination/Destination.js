967aa760860c9c141a405d0805639ab728fd90b6
SURU EMMANUEL <suruaino@gmail.com>
2024-05-12 13:56:08 +0100

    worked on destination component

--- src/components/main/destination/Destination.js
+++ src/components/main/destination/Destination.js
@@ -1,11 +1,16 @@
 import moon from "./destination-assets/image-moon.png";
+import mars from "./destination-assets/image-mars.png";
+import europa from "./destination-assets/image-europa.png";
+import titan from "./destination-assets/image-titan.png";
 import "./destination.css";
 const Destination = () => {
   return (
     <div className="destination">
       <div className="moon-container">
-        <h2><span>01</span> Pick your destination</h2>
-        <div className="content-bx">
+        <h2>
+          <span>01</span> Pick your destination
+        </h2>
+        <div className="destination__content-bx">
           <div className="left">
             <img src={moon} />
           </div>
@@ -37,6 +42,116 @@ const Destination = () => {
           </div>
         </div>
       </div>
+
+      <div className="mars-container">
+        <h2>
+          <span>01</span> Pick your destination
+        </h2>
+        <div className="destination__content-bx">
+          <div className="left">
+            <img src={mars} />
+          </div>
+          <div className="right">
+            <ul className="tab-bx">
+              <li>Moon</li>
+              <li>Mars</li>
+              <li>Europa</li>
+              <li>Titan</li>
+            </ul>
+            <h3>Mars</h3>
+            <p>
+              Don’t forget to pack your hiking boots. You’ll need them to tackle
+              Olympus Mons, the tallest planetary mountain in our solar system.
+              It’s two and a half times the size of Everest!
+            </p>
+            <div className="more-details">
+              <div className="left">
+                <span className="avg-distance">avg. distance</span>
+                <span className="avg-distance__value">225 MIL. km</span>
+              </div>
+
+              <div className="right">
+                <span className="travel-time">est. travel time</span>
+                <span className="avg-distance__value">9 months</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="europa-container">
+        <h2>
+          <span>01</span> Pick your destination
+        </h2>
+        <div className="destination__content-bx">
+          <div className="left">
+            <img src={europa} />
+          </div>
+          <div className="right">
+            <ul className="tab-bx">
+              <li>Moon</li>
+              <li>Mars</li>
+              <li>Europa</li>
+              <li>Titan</li>
+            </ul>
+            <h3>Europa</h3>
+            <p>
+              The smallest of the four Galilean moons orbiting Jupiter, Europa
+              is a winter lover’s dream. With an icy surface, it’s perfect for a
+              bit of ice skating, curling, hockey, or simple relaxation in your
+              snug wintery cabin.
+            </p>
+            <div className="more-details">
+              <div className="left">
+                <span className="avg-distance">avg. distance</span>
+                <span className="avg-distance__value">628 MIL. km</span>
+              </div>
+
+              <div className="right">
+                <span className="travel-time">est. travel time</span>
+                <span className="avg-distance__value">3 years</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="titan-container">
+        <h2>
+          <span>01</span> Pick your destination
+        </h2>
+        <div className="destination__content-bx">
+          <div className="left">
+            <img src={titan} />
+          </div>
+          <div className="right">
+            <ul className="tab-bx">
+              <li>Moon</li>
+              <li>Mars</li>
+              <li>Europa</li>
+              <li>Titan</li>
+            </ul>
+            <h3>Titan</h3>
+            <p>
+            The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.
+            </p>
+            <div className="more-details">
+              <div className="left">
+                <span className="avg-distance">avg. distance</span>
+                <span className="avg-distance__value">1.6 BIL. km</span>
+              </div>
+
+              <div className="right">
+                <span className="travel-time">est. travel time</span>
+                <span className="avg-distance__value">7 years</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 };
